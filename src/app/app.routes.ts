75b896539deb1c3c2ac71b83e41b9d4c5cb1a9b9
Matheus Henrique <matheushale@gmail.com>
2025-09-06 15:26:34 -0400

    adiciona angular material e ajusta components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PessoasListComponent } from './modules/pessoas/containers/pessoas-list/pessoas-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./modules/pessoas/containers/pessoas-list/pessoas-list.component').then(m => m.PessoasListComponent)
+    }
+];
